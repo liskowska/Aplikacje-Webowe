269cb971fadcec57990c8112a4290d54d9cfb69a
liskowska <malgorzata.liskowska@wp.pl>
2025-11-29 13:53:17 +0100

    add stopwatch and password generator

---
+++ Lab3/stopwatch/index2.js
@@ -0,0 +1,44 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset(){
+    if(!isRunning){
+        clearInterval(timer);
+        let startTime = 0;
+        let elapsedTime = 0;
+        display.textContent = "00:00:00:00"
+        let isRunning = false;
+    }
+}
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds =  Math.floor(elapsedTime / 1000 % 60);
+
+    minutes = String(minutes);
+    seconds = String(seconds).padStart(2,"0");
+
+    display.textContent = `${minutes}min ${seconds}sec`
+}
